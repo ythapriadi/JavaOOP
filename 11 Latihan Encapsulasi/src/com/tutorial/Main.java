240727598fabfbfffc3c714447559992c5d0508b
Muhammad Apriadi <muhammadapriadi1999@gmail.com>
2023-07-24 11:02:10 +0700

    Encapsulasi Part 2

--- 11 Latihan Encapsulasi/src/com/tutorial/Main.java
+++ 11 Latihan Encapsulasi/src/com/tutorial/Main.java
@@ -5,11 +5,15 @@ class Player{
     private String name;
     private int baseHealth;
     private int baseAttack;
-    private Armor armor;
-    private Weapon weapon;
-    private int level;
     private int incrementHealth;
     private int incrementAttack;
+    private int level;
+    private int totalDamage;
+    private boolean isAlive;
+
+    //Ini adalah object member
+    private Armor armor;
+    private Weapon weapon;
 
 
     public Player(String name){
@@ -19,18 +23,70 @@ class Player{
         this.level = 1;
         this.incrementHealth = 20;
         this.incrementAttack = 20;
+        this.isAlive = true;
 
+    }
+
+    public String getName(){
+    return this.name;
+}
+
+    public int getHealth(){
+        return this.maxHealth() - this.totalDamage;
     }
 
     public void display(){
         System.out.println("Player     : " + this.name);
         System.out.println("Level      : " + this.level);
-        System.out.println("Max Health : " + this.maxHealth());
-        System.out.println("Attack     : " + this.getAttackPower() + "\n");
-
+        System.out.println("Health     : " + this.getHealth() + "/" + this.maxHealth());
+        System.out.println("Attack     : " + this.getAttackPower());
+        System.out.println("Alive      : " + this.isAlive  + "\n");
     }  
 
-    public void levelUp(){
+    public void attack(Player opponent){
+        //Hitung damage
+        int damage = this.getAttackPower();
+        //print event
+        System.out.println(this.name + " Is attacking " + opponent.getName() + " get " + damage + " Damage");
+        //attack si opponent
+        opponent.defense(damage);
+
+        this.levelUp();
+    }
+
+    public void defense (int damage){
+
+        //receive damage
+        int defensePower = this.armor.getDefencePower();
+        int deltaDamage;
+
+        System.out.println(this.name + " Defense power = " + defensePower);
+        if(damage > defensePower){
+            deltaDamage = damage - defensePower;
+        }else {
+            deltaDamage = 0;
+        }
+        
+        System.out.println("Damage earned " + deltaDamage + "\n");
+        
+        //adding total damage
+        this.totalDamage += deltaDamage;
+
+        //Check is alive
+        if(this.getHealth() <= 0){
+            this.isAlive = false;
+            this.totalDamage = this.maxHealth();
+        }
+
+        this.display();
+        
+    }
+
+     public int getAttackPower(){
+        return this.baseAttack + this.level * this.incrementAttack + this.weapon.getAttack();
+    }
+
+    private void levelUp(){
         this.level++;
     }
 
@@ -45,10 +101,6 @@ class Player{
     public int maxHealth(){
         
         return this.baseHealth + this.level * this.incrementHealth + this.armor.getAddHealth();
-    }
-
-     public int getAttackPower(){
-        return this.baseAttack + this.level * this.incrementAttack + this.weapon.getAttack();
     }
 
 }
@@ -85,6 +137,10 @@ class Armor{
     public int getAddHealth(){
         return this.strength * 10 + this.health;
     }
+
+    public int getDefencePower(){
+        return this.strength * 2;
+    }
 }
 
 public class Main {
@@ -104,5 +160,11 @@ public class Main {
 
         player1.display();
         player2.display();
+
+        player1.attack(player2);
+        player2.attack(player1);
+        player2.attack(player1);
+        player2.attack(player1);
+        
     }
 }
