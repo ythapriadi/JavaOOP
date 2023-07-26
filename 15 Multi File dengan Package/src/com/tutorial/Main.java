193d9af4c4d637b9a01812a709c51e258936ff48
Muhammad Apriadi <muhammadapriadi1999@gmail.com>
2023-07-26 10:31:00 +0700

    Multi File dengan Package

---
+++ 15 Multi File dengan Package/src/com/tutorial/Main.java
@@ -0,0 +1,14 @@
+package com.tutorial;
+
+
+class Main{
+    public static void main(String[] args) {
+       
+        Player player1 = new Player("YTH");
+        Player player2 = new Player("Zabuton");
+        Player player3 = new Player("Kuteki");
+        player1.show();
+        player2.show();
+        player3.show();
+    }
+}
