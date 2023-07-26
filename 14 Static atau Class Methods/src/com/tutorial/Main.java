1ff8575895f9184fd741114ef1f8a801b930bb1f
Muhammad Apriadi <muhammadapriadi1999@gmail.com>
2023-07-26 09:21:40 +0700

    membuat class player

---
+++ 14 Static atau Class Methods/src/com/tutorial/Main.java
@@ -0,0 +1,17 @@
+package com.tutorial;
+
+class Player{
+    private String name;
+
+
+    Player (String name){
+        this.name = name;
+    }
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
