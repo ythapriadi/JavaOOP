08c3518f06b9de4b98e50e7ca409bab9a6fa6138
Muhammad Apriadi <muhammadapriadi1999@gmail.com>
2023-07-26 09:24:57 +0700

    menampilkan nama player

--- 14 Static atau Class Methods/src/com/tutorial/Main.java
+++ 14 Static atau Class Methods/src/com/tutorial/Main.java
@@ -7,11 +7,21 @@ class Player{
     Player (String name){
         this.name = name;
     }
+
+
+    void show(){
+        System.out.println("Player Name = " + this.name);
+    }
 }
 
 
 public class Main {
     public static void main(String[] args) {
         
+        Player player1 = new Player("Saitama");
+        Player player2 = new Player("Deku");
+
+        player1.show();
+        player2.show();
     }
 }
