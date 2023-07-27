d21220eedc564d9fe7b6115bba0062efe7cf52df
Muhammad Apriadi <muhammadapriadi1999@gmail.com>
2023-07-27 09:04:53 +0700

    membuat Player 1,2,3,4

--- 17 Overloading Constructor dan Method/src/com/tutorial/Main.java
+++ 17 Overloading Constructor dan Method/src/com/tutorial/Main.java
@@ -5,9 +5,13 @@ public class Main {
         //Overloading pada constructor
         Player player1 = new Player("YTH");
         Player player2 = new Player();
+        Player player3 = new Player();
+        Player player4 = new Player("Kuteki");
 
         player1.show();
         player2.show();
+        player3.show();
+        player4.show();
         
     }
 }
--- 17 Overloading Constructor dan Method/src/com/tutorial/Player.java
+++ 17 Overloading Constructor dan Method/src/com/tutorial/Player.java
@@ -2,15 +2,18 @@ package com.tutorial;
 
 public class Player {
     private String name;
+    private static int jumlahPlayer;
 
     //overloading contructor
     //Opsi 1
     Player(String name){
+        Player.jumlahPlayer++;
         this.name = name;
     }
 
     Player(){
-        this.name = "Player";
+        Player.jumlahPlayer++;
+        this.name = "Player" + Player.jumlahPlayer;
     }
 
     void show(){
